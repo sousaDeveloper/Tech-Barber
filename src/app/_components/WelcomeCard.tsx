ca8801130f9f8a1420c9949916dfe1c00dda2c84
Matheus Sousa <devlpsousa@gmail.com>
2024-05-25 15:41:06 -0300

    feat: add WelcomeCard Component

---
+++ src/app/_components/WelcomeCard.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
+const CardHello = () => {
+  const { data, status } = useSession();
+
+  return (
+    <div className="bg-[#f59a73] rounded-xl p-2 shadow-xl">
+      {status === "authenticated" ? (
+        <h1 className="font-bold">Olá, {data?.user?.name?.split(" ")[0]}. Vai um corte hoje?</h1>
+      ) : (
+        <h1 className="font-bold">Olá. Faça seu login e agende seu corte!</h1>
+      )}
+
+      <span className="text-sm">Sábado, 25 de maio.</span>
+    </div>
+  );
+};
+
+export default CardHello;
