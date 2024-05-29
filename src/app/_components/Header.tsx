dad75e8649ba97b82d32ae8b9716cf5b77a25a79
Matheus Sousa <devlpsousa@gmail.com>
2024-05-29 11:29:53 -0300

    feat: add UserMenu Component to Header

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,21 +1,13 @@
 "use client";
 
-import { signIn, signOut, useSession } from "next-auth/react";
-import { CalendarDays, CircleUserIcon, LogInIcon, LogOutIcon, ShoppingCartIcon } from "lucide-react";
+import { ShoppingCartIcon } from "lucide-react";
 
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/_components/ui/navigation-menu";
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/_components/ui/sheet";
-import { Avatar, AvatarImage } from "@/_components/ui/avatar";
 import { useEffect, useState } from "react";
 
+import UserMenu from "./UserMenu";
+
 const Header = () => {
-  const { data, status } = useSession();
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
@@ -38,55 +30,10 @@ const Header = () => {
     backdropFilter: "blur(0.3rem)",
   };
 
-  const handleSignInClick = async () => {
-    await signIn();
-  };
-
-  const handleSignOutClick = async () => {
-    await signOut();
-  };
-
   return (
     <header className="flex justify-between items-center p-5 shadow-2xl text-black top-0 z-50 sticky" style={headerStyle}>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger className="bg-transparent">
-              {status === "unauthenticated" ? (
-                <CircleUserIcon size={28} className="cursor-pointer" />
-              ) : (
-                <Avatar>
-                  <AvatarImage src={`${data?.user?.image}`} />
-                </Avatar>
-              )}
-            </NavigationMenuTrigger>
-            <NavigationMenuContent>
-              {status === "unauthenticated" && (
-                <button
-                  className="flex items-center gap-1 font-bold border-b border-solid border-[#bababa] text-left px-1 py-2 text-[0.91rem] hover:bg-[#228992] transition-all duration-300 w-full"
-                  onClick={handleSignInClick}
-                >
-                  <LogInIcon size={16} />
-                  Entrar
-                </button>
-              )}
-              <button className="flex items-center gap-1 px-1 py-2 border-b-2 border-solid text-[0.91rem] hover:bg-[#228992] transition-all duration-300 w-full">
-                <CalendarDays />
-                <span>Agendamentos</span>
-              </button>
-              {status === "authenticated" && (
-                <button
-                  className="flex items-center gap-1 font-bold border-b border-solid border-[#bababa] text-left px-1 py-2 text-[0.91rem] hover:bg-[#228992] transition-all duration-300 w-full"
-                  onClick={handleSignOutClick}
-                >
-                  <LogOutIcon size={16} />
-                  Sair
-                </button>
-              )}
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+      <UserMenu />
+      
       <h1 className="font-bold text-lg text-center">Tech Barber</h1>
 
       <div className="p-1 rounded">
---
+++ src/app/_components/UserMenu.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
+import { Avatar, AvatarImage } from "@/_components/ui/avatar";
+import { CalendarDays, CircleUserIcon, LogInIcon, LogOutIcon } from "lucide-react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/_components/ui/navigation-menu";
+
+const UserMenu = () => {
+  const { data, status } = useSession();
+
+  const handleSignInClick = async () => {
+    await signIn();
+  };
+
+  const handleSignOutClick = async () => {
+    await signOut();
+  };
+
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="bg-transparent">
+            {status === "unauthenticated" ? (
+              <CircleUserIcon size={28} className="cursor-pointer" />
+            ) : (
+              <Avatar>
+                <AvatarImage src={`${data?.user?.image}`} />
+              </Avatar>
+            )}
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            {status === "unauthenticated" && (
+              <button
+                className="flex items-center gap-1 font-bold border-b border-solid border-[#bababa] text-left px-1 py-2 text-[0.91rem] hover:bg-[#228992] transition-all duration-300 w-full"
+                onClick={handleSignInClick}
+              >
+                <LogInIcon size={16} />
+                Entrar
+              </button>
+            )}
+            <button className="flex items-center gap-1 px-1 py-2 border-b-2 border-solid text-[0.91rem] hover:bg-[#228992] transition-all duration-300 w-full">
+              <CalendarDays />
+              <span>Agendamentos</span>
+            </button>
+            {status === "authenticated" && (
+              <button
+                className="flex items-center gap-1 font-bold border-b border-solid border-[#bababa] text-left px-1 py-2 text-[0.91rem] hover:bg-[#228992] transition-all duration-300 w-full"
+                onClick={handleSignOutClick}
+              >
+                <LogOutIcon size={16} />
+                Sair
+              </button>
+            )}
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
+
+export default UserMenu;
