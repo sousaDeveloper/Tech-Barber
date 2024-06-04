5ca36b29f2825278a9ee54c9380b89fe15adc41d
Matheus Sousa <devlpsousa@gmail.com>
2024-06-04 11:46:40 -0300

    feat: add ServiceContent Component

--- src/app/barbershop/[slug]/_components/ServiceItem.tsx
+++ src/app/barbershop/[slug]/_components/ServiceItem.tsx
@@ -15,6 +15,7 @@ import getDayBookings from "../_actions/get-day-bookings";
 
 import { Sheet, SheetClose, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/_components/ui/sheet";
 import { Calendar } from "@/_components/ui/calendar";
+import ServiceContent from "@/app/_components/ServiceContent";
 
 interface ServiceItemProps {
   barbershop: Barbershop;
@@ -96,8 +97,6 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
     return generateDayTimeList(date).filter((time) => {
       const timeHour = Number(time.split(":")[0]);
       const timeMinutes = Number(time.split(":")[1]);
-
-      console.log(`Horas: ${timeHour}, minutos: ${timeMinutes}`);
 
       const booking = dayBookings.find((booking) => {
         const bookingHour = booking.Date.getHours();
@@ -189,28 +188,7 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
               )}
               {date && hour && (
                 <>
-                  <div className="flex flex-col p-2 bg-[#dadada] m-3 rounded-lg gap-2">
-                    <div className="flex justify-between">
-                      <h1 className="text-sm font-bold">{service.name}</h1>
-                      <p className="text-sm">R$ {+service.price},00</p>
-                    </div>
-                    <div className="flex justify-between">
-                      <h1 className="text-sm font-bold">Data</h1>
-                      <p className="text-sm capitalize">{format(date as any, "dd 'de' MMMM", { locale: ptBR })}</p>
-                    </div>
-                    <div className="flex justify-between">
-                      <h1 className="text-sm font-bold">Horário</h1>
-                      <p className="text-sm">{hour}</p>
-                    </div>
-                    <div className="flex justify-between">
-                      <h1 className="text-sm font-bold">Barbearia</h1>
-                      <p className="text-sm">{barbershop.name}</p>
-                    </div>
-                    <div className="flex justify-between">
-                      <h1 className="text-sm font-bold">Local</h1>
-                      <p className="text-sm">{barbershop.address}</p>
-                    </div>
-                  </div>
+                  <ServiceContent service={service} barbershop={barbershop} date={date} hour={hour} />
 
                   <SheetFooter className="mt-2 mx-3">
                     <SheetClose>
