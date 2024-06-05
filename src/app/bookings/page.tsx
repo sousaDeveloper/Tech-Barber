1faa2466419a641b63bbe7b07b9bb97bd5546047
Matheus Sousa <devlpsousa@gmail.com>
2024-06-05 14:50:56 -0300

    feat: add back button if there is no schedule

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { db } from "../_lib/prisma";
 
 import BookingItem from "../_components/BookingItem";
@@ -13,11 +15,20 @@ const BookingsPage = async () => {
 
   return (
     <>
-      {booking.map((booking, index) => (
-        <div key={index} className="mt-0">
-          <BookingItem booking={booking} />
-        </div>
-      ))}
+      {booking.length !== 0 ? (
+        booking.map((booking, index) => (
+          <div key={index} className="mt-0">
+            <BookingItem booking={booking} />
+          </div>
+        ))
+      ) : (
+        <h1 className="px-3 mt-2">
+          Você <span className="font-semi bold">não possui</span> nenhum agendamento.{" "}
+          <Link href="/" className="underline text-[#f59a73] font-bold">
+            Voltar
+          </Link>
+        </h1>
+      )}
     </>
   );
 };
